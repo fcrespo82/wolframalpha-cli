3516b9068c0cfe9f233ee0a13a200732176fabbd
fxcrespo <fxcrespo@al.sp.gov.br>
2018-05-16 17:08:41 -0300

    Migrate to python 3 Removed parser dependency Better results print

--- wolframalpha.py
+++ wolframalpha.py
@@ -1,49 +1,52 @@
 #!/usr/bin/env python
 #coding: utf-8
 
+__version__ = '1.0'
+
 import requests
 import sys
 import re
 import os
-from HTMLParser import HTMLParser
-from urllib2 import quote
+from urllib.parse import quote
 try:
     from colorama import init, Fore, Back, Style
     init()
-except Exception, e:
+except Exception as e:
     class Fore():
-        GREEN = "** "
-        RESET = " **"
+        GREEN = '** '
+        RESET = ' **'
+        YELLOW = ''
 try:
     with open(os.path.expanduser("~/.wolfram_key"), "r") as _file:
         wolfram_alpha_key = "".join(_file.readlines())
-except Exception, e:
-    print("""Invalid API key!
-Get one at https://developer.wolframalpha.com/portal/apisignup.html""")
-    api_key = raw_input('Enter your WolframAlpha API key: ')
+except Exception as e:
+    print('Invalid API key!\nGet one at https://developer.wolframalpha.com/portal/apisignup.html')
+    api_key = input('Enter your WolframAlpha API key: ')
     wolfram_alpha_key = api_key
     with open(os.path.expanduser("~/.wolfram_key"), "w") as _file:
         _file.writelines(api_key)
 
-__version__ = '0.2'
 
 def main():
     if len(sys.argv) > 1:
         query = " ".join(sys.argv[1:])
     else:
-        query = raw_input('Enter query: ')
+        query = input('Enter query: ')
 
-    url = u'http://api.wolframalpha.com/v2/query?input={q}&appid={API_KEY}&format=plaintext'.format(API_KEY = wolfram_alpha_key, q = quote(query))
+    url = f'http://api.wolframalpha.com/v2/query?input={quote(query)}&appid={wolfram_alpha_key}&format=plaintext'
 
     resp = requests.get(url)
 
     for pod in re.findall(r'<pod.+?>.+?</pod>', resp.text, re.S):
         title = re.findall(r'<pod.+?title=[\'"](.+?)[\'"].*>', pod, re.S)
-        parser = HTMLParser()
-        print((Fore.GREEN + parser.unescape("".join(title).strip()) + Fore.RESET).encode(sys.stdout.encoding, 'replace'))
+        print(Fore.GREEN + "".join(title).strip() + Fore.RESET)
         for inner in re.findall(r'<plaintext>(.*?)</plaintext>', pod, re.S):
-            print((parser.unescape(inner.strip()).encode(sys.stdout.encoding, 'replace')))
-        print('')
+            lines = inner.strip().split('\n')
+            print(Fore.YELLOW + lines[0].strip() + Fore.RESET)
+            for l in lines[1:]:
+                print(l.strip())
+        print()
+
 
 if __name__ == '__main__':
     main()
