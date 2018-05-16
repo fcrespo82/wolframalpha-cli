5aba20a1d32ca918b9b2b97a3d1b282dd783b476
fxcrespo <fxcrespo@al.sp.gov.br>
2018-05-16 17:43:41 -0300

    Published version 1.0 in pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = "Fernando Xavier de Freitas Crespo",
     author_email = "fernando82@gmail.com",
     description = ("Command Line Interface to run queries on WolframAlpha"),
-    long_description = "".join(open('README.md').readlines()),
+    long_description = "".join(open('README').readlines()),
     license = "MIT",
     keywords = "wolframalha cli python utility",
     url = "https://github.com/fcrespo82/wolframalpha-cli",
